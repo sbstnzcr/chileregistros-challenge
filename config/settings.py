b77e8bd0d847cf102f075d516e9b14efe080135e
= <=>
2023-05-15 14:24:51 -0400

    Use postgresql instead of sqlite3

--- config/settings.py
+++ config/settings.py
@@ -76,9 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "service",
+            "passfile": ".pg_pass",
+        },
     }
 }
 
